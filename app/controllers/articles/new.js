1f05c69a5cd77dad5b698e4e470d961b67063dc7
Bruno Paulino <brunojppb@gmail.com>
2015-12-31 13:29:11 -0300

    Frieds and Articles resources. nested routes and action bubbling

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  isValid: Ember.computed(
+    'model.description',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.description'));
+      }
+    }
+  ),
+
+  actions: {
+    save() {
+      if(this.get('isValid')) {
+        return true;
+      }
+      else {
+        this.set('errorMessage', 'You have to fill the description');
+        return false;
+      }
+    }
+  }
+
+});
--- app/router.js
+++ app/router.js
@@ -11,6 +11,17 @@ Router.map(function() {
 
     this.route('show', {
       path: ':friend_id'
+      // Nested articles route
+      // So, in this way, we will have site.com/friends/1/articles
+    }, function() {
+      // The resetNamespace key will form the final route friends/1/article
+      // insteand of /articles
+      // Also, we have our article route in the /articles directory, intead
+      //if the friends/show/articles directory tree. This way, we keep the
+      // articles route in the top level directory
+      this.route('articles', {resetNamespace: true}, function(){
+        this.route('new');
+      });
     });
 
     this.route('edit', {
